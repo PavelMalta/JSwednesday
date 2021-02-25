3d379f3dc3b55e3f9bc62c199e0cc2cfc8d95f8b
PavelMalta <shamashov87@mail.ru>
2021-02-25 22:24:15 +0300

    add example in lesson 11

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,3 +1,5 @@
+import {rejects} from "assert";
+
 console.log('lesson 3');
 
 // Event loop
@@ -15,5 +17,50 @@ console.log('lesson 3');
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
 
+let p = new Promise((resolve, reject) => {
+    let a = 10;
+    let b = 15;
+    console.log(a + b);
+    setTimeout(() => {
+        resolve();
+    }, 1000);
+    // ajax(fetch, axios)...
+    reject();
+}) //микротаска не создастся
+
+//Promise methods: then(); catch(); finally()
+
+let promise = new Promise((resolve, rejects) => {
+    // ajax
+    resolve(10)
+})
+promise.then(res => {
+    //ajax
+    return //ajax result
+}, err => {
+}).then(res2 => {
+}, err => {
+})
+
+let testPromise = new Promise((res, rej) => {
+    // ajax
+    rej(10)
+})
+testPromise
+    .then(res => {
+    }, err => {
+        let a = 100500
+        throw 50  // в следующем then попадаем в ветку err
+    })
+    .then(res2 => {
+        console.log(res2)
+    }, err2 => {
+        console.log(err2)
+    })
+
+
+p.then().catch()  // микротаска создастся только при обращении в then или catch
+
 // just a plug
-export default ()=>{};
+export default () => {
+};
