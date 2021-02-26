085e5449d6a346971cdba6e264ce47b233d74edd
PavelMalta <shamashov87@mail.ru>
2021-02-26 12:19:15 +0300

    connect OMDb server in lesson 11

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,13 +3,17 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key = '?apikey=816304bc';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        const query = `${key}&s=${title}`;
+        return axiosInstance.get(query);
     },
     searchFilmsByType: (title: string, type: string) => {
+        const query = `${key}&type=${type}`;
+        return axiosInstance.get(query);
     }
 };
 
