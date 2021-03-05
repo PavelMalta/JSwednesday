ab41752c7d97a872c561017ce844daa6285b6a09
PavelMalta <shamashov87@mail.ru>
2021-03-05 16:07:14 +0300

    add exercise in lesson 12

--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,3 +1,5 @@
+import {log} from "util";
+
 console.log('lesson 4');
 
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
@@ -56,7 +58,75 @@ console.log('lesson 4');
 // Получите результаты работы промисов, объедините свойства объектов
 // и выведите в консоль {name, age, city}
 
+//Task 1
+// setTimeout( () => console.log(1), 0)
+// console.log(2);
+// (()=> console.log(3))();
+// Promise.resolve(console.log(4))  // микро таска не создалась (синхронный код)
+// 2 3 4 1
 
+//Task 2
+// new Promise( (res, rej) => {
+//     console.log(1);
+// });
+// new Promise( (res, rej) => {
+//     setTimeout(() => console.log(2), 0)
+// });
+// Promise.resolve(setTimeout(() => console.log(3), 0));
+// console.log(4);
+// Promise.reject(console.log(5));
+// 1 4 5 2 3
+
+//Task 3
+// (function() {
+//     setTimeout(() => console.log(1), 100);
+// })();
+// console.log(2);
+// new Promise( (res, rej) => {
+//     setTimeout(() => console.log(3), 50);
+// })
+// function f() {
+//     console.log(4)
+// }
+// Promise.resolve(console.log(5));
+// 2 5 3 1
+
+//Task 4
+// function f(num: number) {
+//     console.log(num)
+// };
+// Promise.resolve(1)
+//     .then(f);    //создалась микротаска т.к. отработал метод промиса
+// (function (){
+//     console.log(2)
+// })();
+// console.log(3);
+// new Promise( (res,rej) => {
+//     console.log(4);
+// });
+// setTimeout(f, 0, 5);
+// 2 3 4 1 5
+
+//Task 5
+console.log(1);
+function f() {
+    console.log(2)
+}
+setTimeout( () => {
+    console.log(3);
+    let p = new Promise((res,rej) => {
+        console.log(4);
+        res();
+    });
+    p.then(()=>f())
+}, 0);
+let l = new Promise((res,rej) => {
+    console.log(5);
+    rej()
+});
+l.then(res => console.log(res)).catch(() => console.log(6));
+console.log(7);
+// 1 5 7 6 3 4 2
 
 // just a plug
 export default ()=>{};
